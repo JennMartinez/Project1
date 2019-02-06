1ecf8ffb00a093b18f8777162637d0b2e830db4d
Jennifer M <jennifermartinez@Jennifers-MacBook-Pro.local>
2019-02-06 12:03:45 -0800

    pre-pull

--- assets/javascript/krave.js
+++ assets/javascript/krave.js
@@ -1,89 +1,83 @@
-// / Document Ready //
+// Document Ready //
 $(document).ready(function() {
 
+    var address = "";
+    var city = "";
+    var state = "";
+    var zipCode = 0;
+    var location = "";
+    $(".submit-order").click(function(event){
+        event.preventDefault();
+        console.log("hello jen");
+        yelpQuery();
+        callApi();
+      });
+
+function callApi() {
 // queryURL //
-var queryURL = "https://api.yelp.com/v3/businesses/search?term=store&location" + location + "&categories=liquor";
+    var queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=store&location=" + location + "";
 
-// api key //
-var apiKey = "3X8xKVE39_n-9lZYPfTjEZBbxQAWRqAWj_jhyXfvsXkFMbqc-C1C3wor3d5JUWJqOqGWxb3c4vhgrWpHtjiW_AR8d8z2vpf0Df91ijaUcTuWb4C6gyrbNfNDbwBWXHYx";
+    // api key //
+    var apiKey = "3X8xKVE39_n-9lZYPfTjEZBbxQAWRqAWj_jhyXfvsXkFMbqc-C1C3wor3d5JUWJqOqGWxb3c4vhgrWpHtjiW_AR8d8z2vpf0Df91ijaUcTuWb4C6gyrbNfNDbwBWXHYx";
 
-// "https://api.yelp.com/v3/businesses/search?q=&api_key=3X8xKVE39_n-9lZYPfTjEZBbxQAWRqAWj_jhyXfvsXkFMbqc-C1C3wor3d5JUWJqOqGWxb3c4vhgrWpHtjiW_AR8d8z2vpf0Df91ijaUcTuWb4C6gyrbNfNDbwBWXHYx";
+    // "https://api.yelp.com/v3/businesses/search?q=&api_key=3X8xKVE39_n-9lZYPfTjEZBbxQAWRqAWj_jhyXfvsXkFMbqc-C1C3wor3d5JUWJqOqGWxb3c4vhgrWpHtjiW_AR8d8z2vpf0Df91ijaUcTuWb4C6gyrbNfNDbwBWXHYx";
 
-// AJAX GET request to the queryURL //
-var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://api.yelp.com/v3/businesses/search?location=94521&=liquorstores&term=store&categories=liquor",
-    "method": "GET",
-    "headers": {
-      "Authorization": "Bearer 3X8xKVE39_n-9lZYPfTjEZBbxQAWRqAWj_jhyXfvsXkFMbqc-C1C3wor3d5JUWJqOqGWxb3c4vhgrWpHtjiW_AR8d8z2vpf0Df91ijaUcTuWb4C6gyrbNfNDbwBWXHYx",
-      "cache-control": "no-cache",
-      "Postman-Token": "bd522af8-a7a0-4f76-a52d-f27db838a8c7"
+    // AJAX GET request to the queryURL //
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": queryURL,
+        "method": "GET",
+        "headers": {
+        "Authorization": "Bearer 3X8xKVE39_n-9lZYPfTjEZBbxQAWRqAWj_jhyXfvsXkFMbqc-C1C3wor3d5JUWJqOqGWxb3c4vhgrWpHtjiW_AR8d8z2vpf0Df91ijaUcTuWb4C6gyrbNfNDbwBWXHYx",
+        "cache-control": "no-cache",
+        "Postman-Token": "bd522af8-a7a0-4f76-a52d-f27db838a8c7"
+        }
     }
-  }
   
-$.ajax(settings).done(function (response) {
-    console.log(response);
-});
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+}
 
 // Variable that stores users input for name, email, adddress, city, state, zip code //
-var name = "";
-var email = "";
-var phoneNumber = "";
-var address = "";
-var address2 = "";
-var city = "";
-var state = "";
-var zipCode = 0;
 
 function yelpQuery() {
-// queryURL is the url we'll use to query the API //
+    console.log("I am in the yelp query");
+    // queryURL is the url we'll use to query the API //
     var queryURL = "https://api.yelp.com/v3/businesses/search?term=store&location" + location + "&categories=liquor";
   
-// Begin building an object to contain our API call's query parameters //
-// Set the API key //
+    // Begin building an object to contain our API call's query parameters //
+    // Set the API key //
     var api = { "api-key": "3X8xKVE39_n-9lZYPfTjEZBbxQAWRqAWj_jhyXfvsXkFMbqc-C1C3wor3d5JUWJqOqGWxb3c4vhgrWpHtjiW_AR8d8z2vpf0Df91ijaUcTuWb4C6gyrbNfNDbwBWXHYx" };
   
-// Grab the text the user typed into the NAME input //
-    name = $("#inputName")
-    .val()
-    .trim();
-  
-// Grab the text the user typed into the EMAIL input //   
-    email = $("#inputEmail")      
-    .val()
-    .trim();
-
-// Grab the text the user typed into the EMAIL input //   
-    phoneNumber = $("#inputPhone")      
-    .val()
-    .trim();
-
-// Grab the text the user typed into the ADDRESS input //   
+    // Grab the text the user typed into the ADDRESS input //   
     address = $("#inputAddress")      
     .val()
     .trim();
+    console.log(address);
 
-// Grab the text the user typed into the ADDRESS 2 input //   
-    address2 = $("#inputAddress2")      
-    .val()
-    .trim();
-
-// Grab the text the user typed into the CITY input //   
+    // Grab the text the user typed into the CITY input //   
     city = $("#inputCity")      
     .val()
     .trim();
+    console.log(city);
 
-// Grab the text the user typed into the STATE input //   
+    // Grab the text the user typed into the STATE input //   
     state = $("#inputState")      
     .val()
     .trim();
+    console.log(state);
 
-// Grab the text the user typed into the ZIP CODE input //   
+    // Grab the text the user typed into the ZIP CODE input //   
     zipCode = $("#inputZip")      
     .val()
     .trim();
-  }
+    console.log(zipCode);
+
+    location = address.concat(" ", city, " ", state, " ", zipCode);
+    console.log(location);
+}
 
 // Function to list stores from Yelp //
 function refreshPage(yelp) {
